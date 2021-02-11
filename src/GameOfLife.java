e3a078e1e69d4d4bb66014c0f4c87a4f013240af
Mieszko Mikita <Mieszko.Mikita@gmail.com>
2021-02-11 08:01:40 +0100

    Games Of Life v2

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -24,10 +24,11 @@ public class GameOfLife
             System.out.println("");
             }
             System.out.println("");
-            kolejnageneracja(plansza, szerokosc, wysokosc);
+            kolejnaGeneracja(plansza, szerokosc, wysokosc);
+
         }
 
-            static void kolejnageneracja(int plansza[][],int szerokosc, int wysokosc) {
+            static void kolejnaGeneracja(int plansza[][],int szerokosc, int wysokosc) {
 
                 int [][] symulacja = new int[szerokosc][wysokosc];
 
@@ -52,17 +53,57 @@ public class GameOfLife
                 System.out.println("Kolejna generacja");
                 for (int i = 0; i < szerokosc; i++) {
                     for (int j = 0; j < wysokosc; j++) {
-                        if(symulacja[i][j] ==0)
+                        if (symulacja[i][j] == 0)
                             System.out.print(".");
                         else
                             System.out.print("*");
 
                     }
                     System.out.println();
-
+                }
+                    for (int h = 0; h < 10; h++){
+                        int [][] seria = kolejnaGeneracjaInt(kolejnaGeneracjaInt(symulacja,szerokosc,wysokosc),szerokosc,wysokosc);
+                    }
 
 
                 }
+
+        static int [][] kolejnaGeneracjaInt(int plansza[][],int szerokosc, int wysokosc) {
+
+            int [][] symulacja = new int[szerokosc][wysokosc];
+
+            for (int l = 1; l < szerokosc -1; l++) {
+                for (int m = 1; m < wysokosc -1; m++) {
+
+                    int zyjacysasiad = 0;
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
+                            zyjacysasiad += plansza[l + i][m + j];
+                    zyjacysasiad -=plansza[l][m];
+
+                    if ((plansza[l][m] == 1) &&(zyjacysasiad <2))
+                        symulacja[l][m]=0;
+                    else if ((plansza[l][m] == 1) && (zyjacysasiad >3))
+                        symulacja[l][m] = 0;
+                    else if ((plansza[l][m] == 0) && (zyjacysasiad == 3))
+                        symulacja[l][m] = 1;
+                    else symulacja[l][m] = plansza [l][m];
+
+                    }
+                }
+            System.out.println("Kolejna generacja");
+            for (int i = 0; i < szerokosc; i++) {
+                for (int j = 0; j < wysokosc; j++) {
+                    if (symulacja[i][j] == 0)
+                        System.out.print(".");
+                    else
+                        System.out.print("*");
+
+                }
+                System.out.println();
+
+            }
+        return symulacja;
 
 
 
